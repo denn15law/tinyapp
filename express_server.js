c8a42f2686ca51da094dafa34dd6ed112ab38603
Dennis Law <denn15_law@live.ca>
2021-11-09 04:35:56 +0000

    remove unused code

--- express_server.js
+++ express_server.js
@@ -26,20 +26,6 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: "Hello World" };
-  res.render("hello_world", templateVars);
-});
-
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
 });
-
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-// });
-
-// app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-// });
