b1a3b931281a170b45fd322ffa12c968e15468c7
Dennis Law <denn15_law@live.ca>
2021-11-09 04:27:20 +0000

    render information about single URL

--- express_server.js
+++ express_server.js
@@ -10,12 +10,20 @@ const urlDatabase = {
 };
 
 app.get("/", (req, res) => {
-  res.send("Hello");
+  res.send("Home Page");
 });
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
