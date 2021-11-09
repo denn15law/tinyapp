72718575ed53473a14581a4be1afab5be06f5931
Dennis Law <denn15_law@live.ca>
2021-11-09 04:07:15 +0000

    implement res.render and example ejs pages

--- express_server.js
+++ express_server.js
@@ -13,12 +13,14 @@ app.get("/", (req, res) => {
   res.send("Hello");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { greeting: "Hello World" };
+  res.render("hello_world", templateVars);
 });
 
 app.listen(PORT, () => {
