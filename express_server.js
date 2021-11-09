954815061c2f7793b8680f24daa62b119df38f58
Dennis Law <denn15_law@live.ca>
2021-11-09 04:59:42 +0000

    add html template for url_new

--- express_server.js
+++ express_server.js
@@ -18,6 +18,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {
     shortURL: req.params.shortURL,
