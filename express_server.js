99412e38429875c26858a8440e058766f1d30793
Dennis Law <denn15_law@live.ca>
2021-11-10 19:32:51 +0000

    create template for register page and add new get request in express_server

--- express_server.js
+++ express_server.js
@@ -103,6 +103,13 @@ app.post("/logout", (req, res) => {
   res.redirect("/urls");
 });
 
+app.get("/register", (req, res) => {
+  const templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("register", templateVars);
+});
+
 //LISTENING APP
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
